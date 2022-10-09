70ffa4f647a6e87673f98438b066d2068fa1f178
jiihpeeh <jalonenjussipekka@protonmail.com>
2022-10-09 11:34:04 +0300

    notifications using toast

--- front/src/components/notification.js
+++ front/src/components/notification.js
@@ -1,7 +1,7 @@
 //import { useToast, Button } from '@chakra-ui/react'
 import { createStandaloneToast } from "@chakra-ui/react";
 
-export const notification = (title, description, duration=2000, info='success', isClosable=true ) => {
+export const notification = (title, description, info='success', duration=2000, isClosable=true ) => {
 //export const notification = (title, description ) => {
     const { toast } = createStandaloneToast();
     const statuses = ['success', 'error', 'warning', 'info']
