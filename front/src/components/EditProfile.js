e796f05588b16d8f896f98b3977c68ca37217454
dvz84 <mikkom84@gmail.com>
2022-10-12 14:11:31 +0300

    beginnings of edit profile

--- front/src/components/EditProfile.js
+++ front/src/components/EditProfile.js
@@ -12,12 +12,18 @@ import {
 	DrawerCloseButton,
 	} from '@chakra-ui/react'
 import React from 'react';
+import { useContext } from "react";
+import { SessionContext } from "../contexts/SessionContext";
+
+
 
 // Option for Profile picture? (not necessary)
 
 function EditProfile() {
 	const { isOpen, onOpen, onClose } = useDisclosure()
 	const btnRef = React.useRef()
+
+    const { userInfo, setUserInfo } = useContext(SessionContext);
 
 	return (
 		<>
@@ -31,24 +37,46 @@ function EditProfile() {
 			finalFocusRef={btnRef}
 		>
 		<DrawerOverlay />
-		  <DrawerContent>
+		<DrawerContent>
 			<DrawerCloseButton />
 			<DrawerHeader>Edit profile</DrawerHeader>
-  
 			<DrawerBody>
-			  <input id="name" placeholder='Type here...' />
+			<form 
+				id='edit-profile-form'
+				onSubmit={(e) => {
+                e.preventDefault()
+                console.log('submitted')
+				}}
+            >
+
+			<p>
+				<label htmlFor="firstname">First Name</label></p><p>
+				<input name='firstname' placeholder={userInfo.firstname} />
+			</p>
+
+			<p>
+				<label htmlFor="lastname">Last Name</label></p><p>
+				<input name='lastname' placeholder={userInfo.lastname} />
+			</p>
+			
+			<p>
+				<label htmlFor="email">E-mail</label></p><p>
+				<input name='email' placeholder={userInfo.user} />
+			</p>
+
+            
+			</form>
 			</DrawerBody>
-  
 			<DrawerFooter>
-			  <Button variant='outline' mr={3} onClick={onClose} colorScheme="red">
-				Cancel
-			  </Button>
-			  <Button colorScheme='green'>Save</Button>
+				<Button variant='outline' mr={3} onClick={onClose} colorScheme="red">
+					Cancel
+				</Button>
+				<Button colorScheme='green'>Save</Button>
 			</DrawerFooter>
-		  </DrawerContent>
+		</DrawerContent>
 		</Drawer>
-	  </>
+	</>
 	)
-  }
+}
 
 export default EditProfile;
