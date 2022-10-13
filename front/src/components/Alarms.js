599f2988bd2ce4b68da3a3de7d9fcc0e1e8d05f6
dvz84 <mikkom84@gmail.com>
2022-10-13 15:44:41 +0300

    alarmlist sketch

--- front/src/components/Alarms.js
+++ front/src/components/Alarms.js
@@ -25,7 +25,71 @@ const Alarms = () => {
 
 
 	return (
-		<Text>Hei</Text>
+		<div>List of Alarms
+			<table>
+				<tr>
+					<th>ID</th>
+					<th>Occurence</th>
+					<th>Time</th>
+					<th>Weekday</th>
+					<th>Date</th>
+					<th>Label</th>
+					<th>Devices</th>
+					<th></th>
+				</tr>
+				<tr>
+					<th>1</th>
+					<th>daily</th>
+					<th>08:30</th>
+					<th></th>
+					<th></th>
+					<th>Herätys</th>
+					<th>IOT-herätyskello, kännykkä</th>
+					<th>Edit</th>
+					<th>Delete</th>
+				</tr>
+				<tr>
+					<th>2</th>
+					<th>Weekly</th>
+					<th>16:00</th>
+					<th>Friday</th>
+					<th></th>
+					<th>Alko</th>
+					<th>selain, kännykkä</th>
+					<th>Edit</th>
+					<th>Delete</th>
+
+
+				</tr>
+				<tr>
+					<th>3</th>
+					<th>Once</th>
+					<th>14:00</th>
+					<th>Monday</th>
+					<th>6.12.2022</th>
+					<th>Jorman tuparit</th>
+					<th>selain</th>
+					<th>Edit</th>
+					<th>Delete</th>
+
+
+				</tr>
+				<tr>
+					<th>4</th>
+					<th>yearly</th>
+					<th>12:00</th>
+					<th></th>
+					<th>24.12.</th>
+					<th>joulu</th>
+					<th>selain</th>
+					<th>Edit</th>
+					<th>Delete</th>
+
+				</tr>
+			</table>
+
+
+		</div>
 	)
 }
 
