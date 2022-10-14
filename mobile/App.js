3c44a8b0ba40d2de25a5d22a2bc52a7a40d8e3d7
jiihpeeh <jalonenjussipekka@protonmail.com>
2022-10-14 07:23:08 +0300

    very early react native stuff

--- mobile/App.js
+++ mobile/App.js
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ThemeProvider } from '@rneui/themed';
 
-export default function App() {
+const MyApp = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider>
+      <Button title="Hey!" />
+    </ThemeProvider>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
