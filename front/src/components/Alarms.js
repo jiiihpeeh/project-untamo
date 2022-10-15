9b787d8fe62456914f9ddf1165712ce3b4635321
dvz84 <mikkom84@gmail.com>
2022-10-15 20:03:33 +0300

    Preliminary alarms list

--- front/src/components/Alarms.js
+++ front/src/components/Alarms.js
@@ -2,26 +2,22 @@ import React, { useContext, useEffect } from "react";
 import { SessionContext } from "../contexts/SessionContext"
 import { DeviceContext } from "../contexts/DeviceContext";
 import { useNavigate } from "react-router-dom";
-import { Container, SimpleGrid, Text, Box, Button, Heading} from '@chakra-ui/react';
+import { Container, Button, Heading} from '@chakra-ui/react';
 import {
 	Table,
 	Thead,
 	Tbody,
-	Tfoot,
 	Tr,
 	Th,
 	Td,
-	TableCaption,
 	TableContainer,
-  } from '@chakra-ui/react'
-
+	} from '@chakra-ui/react'
+import { useState } from 'react'
 
 const Alarms = () => {
 	const { token, setToken, userInfo, setUserInfo, sessionStatus, setSessionStatus } = useContext(SessionContext);
 	const { currentDevice, setCurrentDevice, devices, setDevices } = useContext(DeviceContext);
-	
     const navigate = useNavigate();
-
 	useEffect(() =>{
 		if(!sessionStatus){
 			navigate('/login');
@@ -33,69 +29,51 @@ const Alarms = () => {
 		}
 	},[currentDevice])
 
+	var alarmlist = JSON.parse(localStorage['alarms'])
+
+	const [alarms] = useState(alarmlist)
+
+	const renderAlarms = () => {
+		return alarms.map(({ _id, occurence, time, wday, date, label, devices }) => {
+		return <Tr key={_id}>
+		<Td>{_id}</Td>
+		<Td>{occurence}</Td>
+		<Td>{time}</Td>
+		<Td>{wday}</Td>
+		<Td>{date}</Td>
+		<Td>{label}</Td>
+		<Td>{devices}</Td>
+		<Td><Button size='xs'>Edit</Button></Td>
+		<Td><Button colorScheme='red' size='xs'>Delete</Button></Td>
+		</Tr>
+		})
+	}
+
 
 	return (
-	
-	<Container bg='blue.200' maxW='1000px'>
-	<Heading size='sm'>List of Alarms for {localStorage.getItem('screenname')}</Heading>
-	<TableContainer>
-	<Table variant='striped' colorScheme='teal' size='sm' id='gable'>
-	<Thead>
-		<Tr>
-        <Th>ID</Th>
-        <Th>Occurence</Th>
-        <Th isNumeric>Time</Th>
-		<Th>Weekday</Th>
-		<Th>date</Th>
-		<Th>Label</Th>
-		<Th>Devices</Th>
-		<Th></Th>
-		<Th></Th>
-		</Tr>
-    </Thead>
-    <Tbody>
-{/* TEMPORARY */}
-		<Tr>
-		<Td>523</Td>
-        <Td>Once</Td>
-        <Td isNumeric>15:23</Td>
-		<Td>Wednesday</Td>
-		<Td>14.12.2022</Td>
-		<Td>Testings</Td>
-		<Td>Browser, Mobile</Td>
-		<Td><Button size='xs'>Edit</Button></Td>
-		<Td><Button colorScheme='red' size='xs'>Delete</Button></Td>
-		</Tr>
-
-		<Tr>
-		<Td>132</Td>
-        <Td>Daily</Td>
-        <Td isNumeric>09:00</Td>
-		<Td></Td>
-		<Td></Td>
-		<Td>Wake up</Td>
-		<Td>IoT-Clock, Mobile</Td>
-		<Td><Button size='xs'>Edit</Button></Td>
-		<Td><Button colorScheme='red' size='xs'>Delete</Button></Td>
-		</Tr>
-
-		<Tr>
-		<Td>24</Td>
-        <Td>Weekly</Td>
-        <Td isNumeric>16:00</Td>
-		<Td>Friday</Td>
-		<Td></Td>
-		<Td>Alko</Td>
-		<Td>Browser, Mobile</Td>
-		<Td><Button size='xs'>Edit</Button></Td>
-		<Td><Button colorScheme='red' size='xs'>Delete</Button></Td>
-		</Tr>
-
-    </Tbody>
-	</Table>
-</TableContainer><Button size='sm' margin="3px">Add alarm</Button>
+		<Container bg='blue.200' maxW='fit-content'>
+			<Heading size='sm'>List of Alarms for {localStorage.getItem('screenname')}</Heading>
+			<TableContainer>
+				<Table variant='striped' colorScheme='teal' size='sm' className="table-tiny">
+					<Thead>
+						<Tr>
+							<Th>ID</Th>
+							<Th>Occurence</Th>
+							<Th isNumeric>Time</Th>
+							<Th>Weekday</Th>
+							<Th>date</Th>
+							<Th>Label</Th>
+							<Th>Devices</Th>
+							<Th></Th>
+							<Th></Th>
+						</Tr>
+					</Thead>
+				<Tbody id="grable"> 
+					{renderAlarms()} 
+				</Tbody>
+				</Table>
+			</TableContainer><Button size='sm' margin="3px">Add alarm</Button> 
 		</Container>
-	
 	)
 }
 
