78baecfd4df2ba84ad310332758a889d7da7baa7
jiiihpeeh <68808867+jiiihpeeh@users.noreply.github.com>
2022-10-27 23:47:38 +0300

    Update SelectWeekdays.js

--- front/src/components/SelectWeekdays.js
+++ front/src/components/SelectWeekdays.js
@@ -1,24 +1,39 @@
 import { Button, Flex } from "@chakra-ui/react";   
 import React from 'react';
+import { useState } from "react";
 
 const SelectedWeekdays = (props) => {
-    const pushedButton = (day) => {
-        let selectedButton = document.getElementById(`${day}-button`)
-        if (props.days.has(day)){
-            props.days.delete(day);
-            selectedButton.setAttribute('style','background-color: gray');
-
-        }else{
-            props.days.add(day);
-            selectedButton.setAttribute('style','background-color: blue')
+    const weekdays = [["Monday", "Mon"], ["Tuesday","Tue"],
+            ["Wednesday","Wed"], ["Thursday","Thu"], ["Friday","Fri"], 
+            ["Saturday", "Sat"], ["Sunday", "Sun"]];
+    
+    const [buttonColor, setButtonColor ] = useState(() => {
+        let colorMap = new Map()
+        for(const day of weekdays){
+            if(props.days.includes(day)){
+                colorMap.set(day[0], 'blue');
+            }else{
+                colorMap.set(day[0], 'gray');
+            }
         }
-        console.log(props.days);
+        return colorMap;
+    })
+    const pushedButton = (day) => {
+        if (props.days.includes(day)){
+            props.setDays(props.days.filter(selectedDay => selectedDay !== day));
+            buttonColor.set(day,'gray')
+        }else{
+            props.setDays([...props.days, day]); 
+            buttonColor.set(day,'blue')
+        }
+        console.log(buttonColor)
+
     }
 
     const WeekdayButton = (weekday) => {
         return(
             <Button m="1px" 
-                    bgColor="gray" 
+                    bgColor={buttonColor.get(weekday[0])}
                     onClick={() => pushedButton(weekday[0])} 
                     id={`${weekday[0]}-button`} >
             {weekday[1]} 
@@ -27,7 +42,6 @@ const SelectedWeekdays = (props) => {
     }
     
     const WeekdayFlex = () => {
-       let weekdays = [["Monday", "Mon"], ["Tuesday","Tue"], ["Wednesday","Wed"], ["Thursday","Thu"], ["Friday","Fri"], ["Saturday", "Sat"], ["Sunday", "Sun"]];
        let weekDayButtons = [];
        for (const day of weekdays){
           weekDayButtons.push(WeekdayButton(day));
@@ -36,6 +50,8 @@ const SelectedWeekdays = (props) => {
             <Flex>{weekDayButtons}</Flex>
        )
     }
+  
+
     return(
         <WeekdayFlex/>
     )
