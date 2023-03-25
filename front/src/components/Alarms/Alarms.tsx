3c5e4e5d4522291e606d1c4b82f718e61d25e926
jiihpeeh <jalonenjussipekka@protonmail.com>
2023-03-25 11:13:55 +0200

    card fixes

--- front/src/components/Alarms/Alarms.tsx
+++ front/src/components/Alarms/Alarms.tsx
@@ -15,75 +15,75 @@ import { Fade, ScaleFade, Slide, SlideFade, Collapse } from '@chakra-ui/react'
 import { timeToUnits } from './calcAlarmTime'
 
 const Alarms = () => {
-	const containerRef =useRef<HTMLDivElement>(null)
-	const cardColors = useSettings((state)=> state.cardColors)
-	const clock24 = useSettings((state)=> state.clock24)
-	const [devices, viewableDevices] = useDevices(state => 
-		[ state.devices, state.viewableDevices ],  shallow)
-	const [alarms, setToDelete, setToEdit, toggleActivity ] = useAlarms(state => 
-		[ state.alarms, state.setToDelete, state.setToEdit,  state.toggleActivity ],  shallow)
-	const [ setShowEdit, setShowDelete ] = usePopups((state)=> 
-		[state.setShowEditAlarm, state.setShowDeleteAlarm], shallow)
-	const [ showTooltip, setShowTooltip] = useState("")
-	const [ showButtons, setShowButtons] = useState("")
+    const containerRef =useRef<HTMLDivElement>(null)
+    const cardColors = useSettings((state)=> state.cardColors)
+    const clock24 = useSettings((state)=> state.clock24)
+    const [devices, viewableDevices] = useDevices(state => 
+          [ state.devices, state.viewableDevices ],  shallow)
+    const [alarms, setToDelete, setToEdit, toggleActivity ] = useAlarms(state => 
+          [ state.alarms, state.setToDelete, state.setToEdit,  state.toggleActivity ],  shallow)
+    const [ setShowEdit, setShowDelete ] = usePopups((state)=> 
+          [state.setShowEditAlarm, state.setShowDeleteAlarm], shallow)
+    const [ showTooltip, setShowTooltip] = useState("")
+    const [ showButtons, setShowButtons] = useState("")
 
-	const renderCards = () => {
-		let viewableAlarmsSet = new Set<Alarm> ()		
-		let timeAlarmMap = new Map <number, Set<string>>()
-		for(const filtrate of viewableDevices){
-			for(const secondFiltrate of alarms.filter(alarm => alarm.devices.includes(filtrate))){
-				viewableAlarmsSet.add(secondFiltrate)			
-				let timeStamp : number| null
+    const renderCards = () => {
+        let viewableAlarmsSet = new Set<Alarm> ()		
+        let timeAlarmMap = new Map <number, Set<string>>()
+        for(const filtrate of viewableDevices){
+            for(const secondFiltrate of alarms.filter(alarm => alarm.devices.includes(filtrate))){
+                viewableAlarmsSet.add(secondFiltrate)			
+                let timeStamp : number| null
                 try{
                     timeStamp = timeForNextAlarm(secondFiltrate).getTime()
                 }catch(err){
                     timeStamp = null
                 }			                 
-				if(timeStamp && secondFiltrate){
-					let alarmMapStamp = timeAlarmMap.get(timeStamp)
-					if(alarmMapStamp){
-						timeAlarmMap.set(timeStamp, alarmMapStamp.add(secondFiltrate.id) )
-					}else{
-						timeAlarmMap.set(timeStamp, new Set( [ secondFiltrate.id ]))
-					}
-				}
-			}
-		}
-		let viewableAlarms = [...viewableAlarmsSet]
-		let timeMapArray = [...timeAlarmMap.keys()].sort(function(a, b){return a - b})
-		let sortedView : Array<Alarm> = []
-		for(const item of timeMapArray){
-			let timeAlarmMapItem = timeAlarmMap.get(item)
-			if(timeAlarmMapItem){
-				for (const subitem of timeAlarmMapItem){
-					let filtration = viewableAlarms.filter(alarm => alarm.id === subitem)[0]
-					if(filtration){
-						sortedView.push(filtration)
-					}
-				}
-			}
-		}
+                if(timeStamp && secondFiltrate){
+                    let alarmMapStamp = timeAlarmMap.get(timeStamp)
+                    if(alarmMapStamp){
+                        timeAlarmMap.set(timeStamp, alarmMapStamp.add(secondFiltrate.id) )
+                    }else{
+                        timeAlarmMap.set(timeStamp, new Set( [ secondFiltrate.id ]))
+                    }
+                }
+            }
+        }
+        let viewableAlarms = [...viewableAlarmsSet]
+        let timeMapArray = [...timeAlarmMap.keys()].sort(function(a, b){return a - b})
+        let sortedView : Array<Alarm> = []
+        for(const item of timeMapArray){
+            let timeAlarmMapItem = timeAlarmMap.get(item)
+            if(timeAlarmMapItem){
+                for (const subitem of timeAlarmMapItem){
+                    let filtration = viewableAlarms.filter(alarm => alarm.id === subitem)[0]
+                    if(filtration){
+                       sortedView.push(filtration)
+                    }
+                }
+            }
+        }
 
-		const timeTooltip = (id: string) =>{
-			const timeMs = timeToNextAlarm(alarms.filter( item =>  item.id === id  )[0])
-			const time = timeToUnits(Math.round(timeMs/1000))
-			setShowTooltip(`${time.days} days ${timePadding(time.hours)}:${timePadding(time.minutes)}:${timePadding(time.seconds)}`)
-		} 
+        const timeTooltip = (id: string) =>{
+            const timeMs = timeToNextAlarm(alarms.filter( item =>  item.id === id  )[0])
+            const time = timeToUnits(Math.round(timeMs/1000))
+            setShowTooltip(`${time.days} days ${timePadding(time.hours)}:${timePadding(time.minutes)}:${timePadding(time.seconds)}`)
+        } 
         const occurenceInfo = (occurence: AlarmCases, weekdays: Array<WeekDay>, date: string)=> {
             switch(occurence){
                 case AlarmCases.Weekly:
                     return(
                         <Box>
                         <Heading 
-							size='xs' 
-							textTransform='uppercase'
-						>
+                            size='xs' 
+                            textTransform='uppercase'
+                        >
                             Weekdays
                         </Heading>
                         <Text 
-							pt='2' 
-							fontSize='sm'
-						>
+                            pt='2' 
+                            fontSize='sm'
+                        >
                             {weekdayDisplay(weekdays, date)}
                         </Text>
                         </Box>)
@@ -91,9 +91,9 @@ const Alarms = () => {
                     return(
                         <Box>
                         <Heading 
-							size='xs' 
-							textTransform='uppercase'
-						>
+                            size='xs' 
+                            textTransform='uppercase'
+                        >
                             Date
                         </Heading>
                         <Text pt='2' fontSize='sm'>
@@ -101,91 +101,91 @@ const Alarms = () => {
                         </Text>
                     </Box>
                     )
-				case AlarmCases.Daily:
-					return(
-						<Box>
-							<Heading 
-								size='xs' 
-								textTransform='uppercase'
-							>
-								Weekdays
-							</Heading>
-							<Text 
-								pt='2' 
-								fontSize='sm'
-							>
-								{weekdayDisplay(Object.values(WeekDay).filter((item) => item), date)}
-							</Text>
+                case AlarmCases.Daily:
+                    return(
+                        <Box>
+                            <Heading 
+                                size='xs' 
+                                textTransform='uppercase'
+                            >
+                                Weekdays
+                            </Heading>
+                            <Text 
+                                pt='2' 
+                                fontSize='sm'
+                            >
+                                {weekdayDisplay(Object.values(WeekDay).filter((item) => item), date)}
+                            </Text>
                         </Box>
-					)
+                )
                 case AlarmCases.Yearly:
                     return(
                         <Box>
                         <Heading 
-							size='xs' 
-							textTransform='uppercase'
-						>
+                            size='xs' 
+                            textTransform='uppercase'
+                        >
                             Date
                         </Heading>
                         <Text 
-							pt='2' 
-							fontSize='sm'
-						>
+                            pt='2' 
+                            fontSize='sm'
+                        >
                             {`${date} ${weekdayDisplay(weekdays, date)}`}
                         </Text>
                     </Box>
                     )
             }
         }
-		const getTime = (time: string)=>{
-			let string12 = ""
-			if(!clock24){
-				let fmt = time24hToTime12h(time)
-				return (<HStack><Text>{fmt.time}  <Text fontSize='sm'>{fmt['12h']}</Text></Text></HStack>)
-			}
-			return (<Text>{time}</Text>)
-		}
-		return sortedView.map(({ id, occurence, time, weekdays, date, label, devices, active },key) => {
-			return (
+        const getTime = (time: string)=>{
+            let string12 = ""
+            if(!clock24){
+                let fmt = time24hToTime12h(time)
+                return (<HStack><Text>{fmt.time}  <Text fontSize='sm'>{fmt['12h']}</Text></Text></HStack>)
+            }
+            return (<Text>{time}</Text>)
+        }
+        return sortedView.map(({ id, occurence, time, weekdays, date, label, devices, active },key) => {
+            return (
                     <Card
                         key={key}
                         backgroundColor={(!active)?cardColors.inactive:((key % 2 === 0)?cardColors.odd:cardColors.even)}
                         onMouseLeave={()=>{setShowButtons("")}}
                         onMouseEnter={() => { setShowButtons(id); timeTooltip(id)}}
                         mb={"5px"}
-						id={`alarmCardContainer-${key}`}
-						size={"sm"}
+                        id={`alarmCardContainer-${key}`}
+                        size={"sm"}
                     >                        
                         <CardBody>
                             <Tooltip 
-								label={showTooltip}
-							>                               
+                               label={showTooltip}
+                            >                               
                                 <CardHeader >
                                     {`${capitalize(occurence)}: `} {<Text as="b">{label}</Text>}
                                 </CardHeader> 
                             </Tooltip>
                             <HStack 
-								divider={<StackDivider />}
-							>
+                               divider={<StackDivider />}
+                            >
                             <Box>
                                 <Heading 
-									size='xl' 
-									textTransform='uppercase'
-								>
+                                    size='xl' 
+                                    textTransform='uppercase'
+                                >
                                     {getTime(time)}
                                 </Heading>
                             </Box>
                             <Box>
                                 <Heading 
-									size='xs' 
-									textTransform='uppercase'
-								>
+                                    size='xs' 
+                                    textTransform='uppercase'
+                                >
                                     Devices
                                 </Heading>
                                 <Text 
-									pt='2' 
-									fontSize='sm'
-								>
+                                    pt='2' 
+                                    fontSize='sm'
+                                >
                                     {mapDeviceIDsToNames(devices)}
                                 </Text>
                             </Box>
@@ -193,18 +193,18 @@ const Alarms = () => {
 
                             </HStack>
                             <Collapse 
-								in={showButtons === id} 
-								animateOpacity={true}  
-							>
+                                in={showButtons === id} 
+                                animateOpacity={true}  
+                            >
                                 <Flex 
-									mt={"10px"}
-								>
+                                    mt={"10px"}
+                                >
                                     <Box>
                                         <Heading 
-											size='xs' 
-											textTransform='uppercase' 
-											mb="4px"
-										>
+                                            size='xs' 
+                                            textTransform='uppercase' 
+                                            mb="4px"
+                                        >
                                             Edit
                                         </Heading>
                                         <IconButton 
@@ -219,10 +219,10 @@ const Alarms = () => {
                                     <Spacer/>
                                     <Box>
                                         <Heading 
-											size='xs' 
-											textTransform='uppercase' 
-											mb="4px"
-										>
+                                            size='xs' 
+                                            textTransform='uppercase' 
+                                            mb="4px"
+                                        >
                                             Active
                                         </Heading>
                                         <Switch 
@@ -236,10 +236,10 @@ const Alarms = () => {
                                     <Spacer />
                                     <Box >
                                         <Heading 
-											size='xs' 
-											textTransform='uppercase' 
-											mb="4px"
-										>
+                                            size='xs' 
+                                            textTransform='uppercase' 
+                                            mb="4px"
+                                        >
                                             Delete
                                         </Heading>
                                         <IconButton 
@@ -255,46 +255,48 @@ const Alarms = () => {
                             </Collapse>
                         </CardBody>
                     </Card>
-		)})
+                )
+            }
+        )
     }
 
-	const mapDeviceIDsToNames = (deviceIDs : Array<string>) =>{
-		let filteredDevices = devices.filter(device => deviceIDs.includes(device.id))
-		let filteredDeviceNames: Array<string> = [] 
-		for(const dev of filteredDevices){
-			filteredDeviceNames.push(dev.deviceName)
-		}
-		return filteredDeviceNames.join(", ")
-	}
+    const mapDeviceIDsToNames = (deviceIDs : Array<string>) =>{
+        let filteredDevices = devices.filter(device => deviceIDs.includes(device.id))
+        let filteredDeviceNames: Array<string> = [] 
+        for(const dev of filteredDevices){
+           filteredDeviceNames.push(dev.deviceName)
+        }
+        return filteredDeviceNames.join(", ")
+    }
 
-	const weekdayDisplay = (days: Array<WeekDay>, date:string) => {
-		let dayArr = dayContinuationDays(days)
-		let subList: Array<string> = []  
-		for(const outer of dayArr){
-			subList.push(outer.join('-'))
-		}
-		let daysFormat = subList.join(', ')
-		if(daysFormat.length === 0){
-			if(date.length > 0){
-				daysFormat = numberToWeekDay(stringToDate(date).getDay())
-			}
-		}
-		return daysFormat
-	}
+    const weekdayDisplay = (days: Array<WeekDay>, date:string) => {
+        let dayArr = dayContinuationDays(days)
+        let subList: Array<string> = []  
+        for(const outer of dayArr){
+           subList.push(outer.join('-'))
+        }
+        let daysFormat = subList.join(', ')
+        if(daysFormat.length === 0){
+            if(date.length > 0){
+                daysFormat = numberToWeekDay(stringToDate(date).getDay())
+            }
+        }
+        return daysFormat
+    }
 
-	return (
-			<>
-				<Container 
-					id={`alarmCardContainer`} 
-					ref={containerRef}
-				>
+    return (
+            <>
+                <Container 
+                    id={`alarmCardContainer`} 
+                    ref={containerRef}
+                >
                     {renderCards()}
-				</Container>
-				<AddAlarmButton 
-					mounting={containerRef}
-				/>
-			</>
-		)
+                </Container>
+                <AddAlarmButton 
+                    mounting={containerRef}
+                />
+            </>
+        )
 }
 
 export default Alarms
