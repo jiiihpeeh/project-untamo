91d7fa3efb4a0a80391d26fbd15e4a6afb38661b
jiiihpeeh <68808867+jiiihpeeh@users.noreply.github.com>
2023-05-22 11:02:42 +0300

    Delete back directory

--- back/modules/form_screenname.js
+++
@@ -1,8 +0,0 @@
-const formScreenName = (body) => {
-    if (body.firstName.length === 0 &&  body.lastName.length === 0){
-        return body.email.split('@')[0];
-    }
-    return `${body.firstName} ${body.lastName}`;
-}
-
-module.exports = formScreenName
--- back/modules/tstamppi.js
+++
@@ -1,6 +0,0 @@
-const tStamppi = () => {
-	var d = new Date();
-	var datetime = d.toLocaleString()+": "
-	return datetime
-}
-module.exports = tStamppi
