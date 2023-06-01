00f387ab37ffe5d87500b4a6a7fc82c2fb43d419
jiihpeeh <jalonenjussipekka@protonmail.com>
2023-06-01 06:38:09 +0300

    weekday reformat as a number

--- front/src/components/Alarms/AlarmComponents/alarmStates.ts
+++ front/src/components/Alarms/AlarmComponents/alarmStates.ts
@@ -11,14 +11,10 @@ export const enum Direction {
     Decrease="dec"
 }
 function toggleWeekdays(day:number, weekdays:number) {
-    const bitmask = 1 << day; // Bitmask for the specified weekday
-  
-    // Toggle the specified weekday
-    weekdays ^= bitmask;
+    const bitmask = 1 << day
+    weekdays ^= bitmask
     return weekdays
 }
-
-
 
 const fingerprint = () => useLogIn.getState().fingerprint
 
@@ -166,7 +162,7 @@ const useAlarm = create<AlarmStates>((set, get) => (
             set(
                 {
                     date: day,
-                    weekdays:  toggleWeekdays(day.getDay(),0)//[numberToWeekDay(day.getDay())]
+                    weekdays:  toggleWeekdays(day.getDay()-1,0)//[numberToWeekDay(day.getDay())]
                 }
             )
         },
@@ -235,7 +231,7 @@ const useAlarm = create<AlarmStates>((set, get) => (
         onAddOpen: () => set (
             {
                 occurrence: AlarmCases.Weekly,
-                weekdays: toggleWeekdays(new Date().getDay(),0),//,[ numberToWeekDay(new Date().getDay()) ],
+                weekdays: toggleWeekdays(new Date().getDay()-1,0),//,[ numberToWeekDay(new Date().getDay()) ],
                 label: "Alarm",
                 time: alarmTimeInit(),
                 date: new Date(),
@@ -283,7 +279,7 @@ const useAlarm = create<AlarmStates>((set, get) => (
             if(alarm.occurrence === AlarmCases.Once){
                 set (
                     {
-                        weekdays: toggleWeekdays(get().date.getDay(),0)//[ numberToWeekDay(get().date.getDay()) ]
+                        weekdays: toggleWeekdays(get().date.getDay()-1,0)//[ numberToWeekDay(get().date.getDay()) ]
                     }
                 )
             }
