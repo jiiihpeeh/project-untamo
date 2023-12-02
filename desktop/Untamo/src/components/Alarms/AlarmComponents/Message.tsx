8474a492b7be0aa43955f936d17185fd4d5a50cb
jiihpeeh <jalonenjussipekka@protonmail.com>
2023-12-02 11:22:52 +0200

    admin

--- desktop/Untamo/src/components/Alarms/AlarmComponents/Message.tsx
+++ desktop/Untamo/src/components/Alarms/AlarmComponents/Message.tsx
@@ -5,52 +5,49 @@ import Picker from '@emoji-mart/react'
 import  useSettings  from "../../../stores/settingsStore"
 import  useEmojiStore, { Skin }  from "../../../stores/emojiStore"
 
-const Message = () => {
-    const label= useAlarm((state)=> state.label)
-    const setLabel = useAlarm((state)=> state.setLabel)
-    const [ showEmoji, setShowEmoji ]  = useState(false)
-    const isLight = useSettings((state)=>state.isLight)
-    const data = useEmojiStore((state)=>state.getEmojiData)()
+function Message() {
+    const label = useAlarm((state) => state.label)
+    const setLabel = useAlarm((state) => state.setLabel)
+    const [showEmoji, setShowEmoji] = useState(false)
+    const isLight = useSettings((state) => state.isLight)
+    const data = useEmojiStore((state) => state.getEmojiData)()
 
-    function onEmojiSelect(emoji :Skin) {
-        setLabel(label+emoji.native)
+    function onEmojiSelect(emoji: Skin) {
+        setLabel(label + emoji.native)
     }
 
-    return(
+    return (
         <Center>
-            <Flex 
+            <Flex
                 m={"1%"}
             >
                 <FormLabel
-                    onMouseDown={e=>e.preventDefault()}
+                    onMouseDown={e => e.preventDefault()}
                 >
                     Message
                 </FormLabel>
                 <VStack>
                     <HStack>
-                        <Input 
-                            value={label} 
-                            onChange={(e) => setLabel(e.target.value)} 
-                        />
+                        <Input
+                            value={label}
+                            onChange={(e) => setLabel(e.target.value)} />
                         <Button
-                            onClick={()=>setShowEmoji(!showEmoji)}
+                            onClick={() => setShowEmoji(!showEmoji)}
                         >
                             ⏰
                         </Button>
                     </HStack>
-                    {  showEmoji ?
-                    <Picker 
-                        data={data} 
-                        onEmojiSelect={onEmojiSelect}
-                        theme={isLight ? 'light' : 'dark'}
-                        width={300}
-                    />
-                    :
-                    <></>
-                    }
+                    {showEmoji ?
+                        <Picker
+                            data={data}
+                            onEmojiSelect={onEmojiSelect}
+                            theme={isLight ? 'light' : 'dark'}
+                            width={300} />
+                        :
+                        <></>}
                 </VStack>
             </Flex>
-        </Center>          
+        </Center>
     )
 }
 export default Message
